fe079516931c784eddbe30efe0d890f4aacf8f90
MichaeLaudrup <68240896+MichaeLaudrup@users.noreply.github.com>
2021-10-13 16:27:21 +0200

    Add files via upload

---
+++ P3_RestFullAPIconJPA/target/classes/data.sql
@@ -0,0 +1,9 @@
+insert into students(id, name, email, disabled) values(1,'Bethanie G. Addams','Bethanie.Adams@oneself.io', false);
+
+insert into students(id, name, email, disabled) values(2,'John F. McReally','John.McReally@oneself.io', true);
+
+insert into students(id, name, email, disabled) values(3,'Claudia M. Kirshner','Claudia.Kirshner@oneself.io', true);
+
+insert into students(id, name, email, disabled) values(4,'Nelson B. Ransom','Nelson.Ransom@oneself.io', false);
+
+insert into students(id, name, email, disabled) values(5,'Roxane L. Russell','Roxane.Russell@oneself.io', true);
